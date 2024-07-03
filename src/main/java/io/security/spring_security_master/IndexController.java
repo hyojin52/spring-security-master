7ab7556bbdce16f8d56b8bbab760f81d2c25b99d
hyojin <dongurami.kim@gmail.com>
2024-07-03 16:17:54 +0900

    anonymous 설정 추가

---
+++ src/main/java/io/security/spring_security_master/IndexController.java
@@ -0,0 +1,37 @@
+package io.security.spring_security_master;
+
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+  
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
+  
+  @GetMapping("/anonymous")
+  public String anonymous() {
+    return "anonymous";
+  }
+  
+  @GetMapping("/anonymousContext")
+  public String anonymousContext(@CurrentSecurityContext SecurityContext context) {
+    return context.getAuthentication().getName();
+  }
+  
+  @GetMapping("/authentication")
+  public String authentication(Authentication authentication) {
+    if(authentication instanceof AnonymousAuthenticationToken) {
+      return "anonymous";
+    } else {
+      return "not anonymous";
+    }
+  }
+}
